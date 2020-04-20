6232e2b0f67fb21136fabda3a0914415154b9636
Arucard <roquelson@gmail.com>
2020-04-20 20:21:50 -0300

    Projeto NodeJS da RocketSeat

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const routes = express.Router();
+routes.use(cors());
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
+
+module.exports = routes;
